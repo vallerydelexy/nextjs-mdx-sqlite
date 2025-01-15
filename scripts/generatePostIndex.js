c2902dacd0525a7c3927731c1778645c2c880651
Rizki Aprita <vallerydelexy@gmail.com>
2025-01-16 01:50:03 +0700

    better index.

--- scripts/generatePostIndex.js
+++ scripts/generatePostIndex.js
@@ -2,10 +2,9 @@ import { statSync, writeFileSync } from "fs";
 import { join, extname, basename } from "path";
 import { getMDXFiles, readMDXFile, generateExcerpt } from "../src/utils/helper.js";
 
-
 function generatePostIndex(dir) {
   const mdxFiles = getMDXFiles(dir);
-  return mdxFiles.map((file) => {
+  const posts = mdxFiles.map((file) => {
     const { metadata, content } = readMDXFile(join(dir.toString(), file));
     const slug = basename(file, extname(file));
     const trimmedContent = generateExcerpt(content);
@@ -21,15 +20,18 @@ function generatePostIndex(dir) {
       updatedAt
     };
   });
+  posts.sort((a, b) => b.updatedAt - a.updatedAt);
+
+  return posts;
 }
 
 async function main() {
-    const posts = generatePostIndex(join(process.cwd(), "posts"));
-    writeFileSync("src/app/blog/posts.json", JSON.stringify(posts));
+  const posts = generatePostIndex(join(process.cwd(), "posts"));
+  writeFileSync("src/app/blog/posts.json", JSON.stringify(posts, null, 2));
 }
 
 main()
   .catch((e) => {
     console.error(e);
     process.exit(1);
-  })
+  });
