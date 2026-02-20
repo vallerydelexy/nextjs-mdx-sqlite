14275000b70cd87e4e2933a5ecd0087aa693e72e
rizkiaprita <rizkiaprita@clearscope.solutions>
2026-02-20 22:56:11 +0700

    upgrade react-mdx-remote

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -25,32 +25,34 @@ export default async function Blog({ searchParams }) {
       <h1 className="text-3xl font-bold mb-4 text-center">Blog posts</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12">
         {posts.map((post) => (
-          <Card key={post.id} className="overflow-hidden">
-            <Link href={`/${post.slug}`}>
-              <Image
-                src={`/${post.cover}`}
-                alt={post.title}
-                width={600}
-                height={400}
-                className="w-full h-48 object-cover"
-              />
-              <CardHeader>
-                <CardTitle className="text-xl font-semibold line-clamp-2">
-                  {post.title}
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="text-muted-foreground mb-4 line-clamp-3">
-                  {post.content.trim().split(" ").slice(0, 20).join(" ")}
-                </p>
-                <div className="flex items-center justify-between">
-                  <span className="text-sm text-muted-foreground">
-                    {post.date}
-                  </span>
-                </div>
-              </CardContent>
-            </Link>
-          </Card>
+          <div key={post.id}>
+            <Card className="overflow-hidden">
+              <Link href={`/${post.slug}`}>
+                <Image
+                  src={`/${post.cover}`}
+                  alt={post.title}
+                  width={600}
+                  height={400}
+                  className="w-full h-48 object-cover"
+                />
+                <CardHeader>
+                  <CardTitle className="text-xl font-semibold line-clamp-2">
+                    {post.title}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-muted-foreground mb-4 line-clamp-3">
+                    {post.content.trim().split(" ").slice(0, 20).join(" ")}
+                  </p>
+                  <div className="flex items-center justify-between">
+                    <span className="text-sm text-muted-foreground">
+                      {post.date}
+                    </span>
+                  </div>
+                </CardContent>
+              </Link>
+            </Card>
+          </div>
         ))}
       </div>
       <BlogPagination page={page} totalPages={totalPages} />
